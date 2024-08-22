f6980746accab75bb019d67f57fe6daf14aaf791
ManishGriffith <140369182+ManishGriffith@users.noreply.github.com>
2024-08-22 14:12:54 +1000

    updated navbar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,58 +1,65 @@
 
 import Link from 'next/link';
+import { useState } from "react";
 
+const tabsData = [
+  {
+    label: "Style",
+    links: "/style"
 
+  },
+  {
+    label: "Template",
+    links: "/template"
+  },
+  {
+    label: "Setting",
+    links: "/setting"
+  },
+  
+];
 
 const NavBar = () => {
 
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
 
   return (
 
     <div>
-      <div className="flex justify-between items-center bg-[#50A2A7] p-5">
+      <div className="flex justify-between items-center bg-[#31dce2] p-5">
         <Link href = "/">
         <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
+          className="border-solid border-2 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
         >
         Cancel
         </button>
         </Link>
 
         <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg"
-        //   onClick={() => handleNavigation('')}  
+          className="border-solid border-2 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg" 
         >
           Save
         </button>
       </div>
-      <ul className="flex justify-around w-1/3 p-2 mx-3">
-          <li className="flex">
-            <Link href ="/style">
-              <button
-              className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg border border-gray-900 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85]"
-              >Style
-              </button>
-            </Link>
-          </li>
-          <li className="flex">
-            <Link href ="/template">
-              <button
-              className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg border border-gray-900 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85]"
-              >
-              Template
-              </button>
-            </Link>
-          </li>
-          <li className="flex">
-            <Link href ="/setting">
-              <button
-              className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg border border-gray-900 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85]"
-              >
-              Settings
-              </button>
-            </Link>
-          </li>
-      </ul>
+  
+      <div className="flex justify-start ">
+        <div className="flex space-x-3 w-1/3">
+          {/* Loop through tab data and render button each button */}
+          {tabsData.map((tab, index) => {
+            return (
+              //changes the link for the tabs
+              <Link href ={tab.links} key={index} className="w-full">
+                <button key={index} className={`w-full py-4 text-lg border-b-4 transition-colors duration-300 
+                ${index === activeTabIndex ? "border-teal-500" : "border-transparent hover:border-gray-200"}`}
+                // Change the active tab on click.
+                onClick={() => setActiveTabIndex(index)} >
+                  {tab.label}
+                </button>
+              </Link>
+              );
+            })}
+        </div>
+       </div>
     </div>
   );
 }
