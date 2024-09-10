81b22eb3eb636219cec56bedf2b0257cf2969dd0
ManishGriffith <140369182+ManishGriffith@users.noreply.github.com>
2024-09-10 12:51:39 +1000

    changed nav names and style page layout

--- src/app/style/page.jsx
+++ src/app/style/page.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function StylePage() {
   return (
-    <div>StylePage</div>
+
+      <div className="flex min-h-screen bg-gray-100">
+       
+        <div className="w-1/3 p-4 bg-white border-r border-gray-300 rounded-xl">
+        <h1>Style page</h1>
+        
+
+        
+        </div>
+      </div>
   )
 }
