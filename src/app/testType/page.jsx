74e20ba0bd12241faca943f40fdc3588b44908f1
zgibbs03 <127379001+zgibbs03@users.noreply.github.com>
2024-09-13 14:34:39 +1000

    Updated Select Quiz Type Page
    Updated the Quiz page still needs navbar, only frontend changes made no linking to backend

--- src/app/testType/page.jsx
+++ src/app/testType/page.jsx
@@ -6,10 +6,22 @@ import { useState } from "react";
 
 export default function testType() {
     const [activeButton, setActiveButton] = useState(null);
+    const [activeCheckbox, setActiveCheckbox] = useState({
+        survey: false,
+        quiz: false,
+        personalityTest: false,
+    });
 
     // Handler to set the active button
     const handleButtonClick = (buttonIndex) => {
         setActiveButton(buttonIndex);
+
+        //Sets the active checkbox on button click/active button
+        setActiveCheckbox ({
+            survey: buttonIndex === 1 ? true : false,
+            quiz: buttonIndex === 2 ? true : false,
+            personalityTest: buttonIndex === 3 ? true : false,
+        });
     };
 
   return (
@@ -18,34 +30,125 @@ export default function testType() {
             <h1 className="text-4xl font-bold">Choose the Test Type</h1>
         </header>
         
-        <div className="flex flex-1 pt-6">
+        <div className="flex flex-1 pt-6 px-20">
+
+            {/* Survey */} 
             <button
-            className={`flex-1 m-2 p-1 rounded ${activeButton === 1 ? "bg-blue-500" : "border-r-transparent"}`}
-            onClick={() => handleButtonClick(1)}
-            >
-                <div className={`h-full w-full p-4 rounded ${activeButton === 1 ? "bg-gray-200" : "bg-gray-200 text-black"}`}>
-                <h2 className='font-bold'>Sruvey</h2>
-                <p>A Tool used to gather information, opinions, or feedback form praticipants through a series of quesitions.</p>
+            className={`flex-1 m-2 p-1 ${activeButton === 1 ? "bg-blue-500" : "border-r-transparent"}`}
+            style={{ height: '600px', display: 'flex', flexDirection: 'column', borderRadius: '53px'}}
+            onClick={() => handleButtonClick(1)}>
+                <div className={`h-full w-full p-4 flex flex-col justify-between ${activeButton === 1 ? "bg-gray-200" : "bg-gray-200 text-black"}`}
+                style={{borderRadius: '50px'}}>
+                    <div style={{paddingLeft: "10px", paddingRight: "10px"}}>
+                        <div className='flex justify-between items-center' style={{paddingTop: '30px', paddingRight: '20px'}}>
+                            <h2 className='font-bold text-left' style={{ fontSize: '20px', paddingBottom: '20px'}}>Survey</h2>
+                            <input type='checkbox' className='form-checkbox' checked={activeCheckbox.survey} readOnly></input>
+                        </div>
+                        <p className='text-left'>A survey is a tool used to gather information, opinions, or feedback from participants through a series of questions.
+                        The collected responses help to understand their preferences, behaviours, or experiences, and are often used 
+                        to guide decision-making or improve services.
+                        </p>
+                    </div>
+
+                    {/* Complexity + Engagement Metre */}
+                    <div>
+                        <div className='flex justify-left items-center mt-auto space-x-1' style={{paddingBottom: '15px', paddingLeft: '15px'}}>
+                            <p style={{paddingRight: '5px'}}>Complexity</p>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                        </div>
+                        <div className='flex justify-left items-center mt-auto space-x-1' style={{paddingBottom: '30px', paddingLeft: '15px'}}>
+                            <p style={{paddingRight: '5px'}}>Engagement</p>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                        </div>
+                    </div>
                 </div>
             </button>
 
+            {/* Quiz */} 
             <button
-            className={`flex-1 m-2 p-1 rounded ${activeButton === 2 ? "bg-blue-500" : "border-r-transparent"}`}
-            onClick={() => handleButtonClick(2)}
-            >
-                <div className={`h-full w-full p-4 rounded ${activeButton === 2 ? "bg-gray-200" : "bg-gray-200 text-black"}`}>
-                <h2 className='font-bold' >Score Test</h2>
-                <p>You will set up a series of quizzes with designbated right or wrong answers. At the end, the test will calculate how many quesitons the user answered correctly and display the total score as the result.</p>
+            className={`flex-1 m-2 p-1 ${activeButton === 2 ? "bg-blue-500" : "border-r-transparent"}`}
+            style={{ height: '600px', display: 'flex', flexDirection: 'column', borderRadius: '53px'}}
+            onClick={() => handleButtonClick(2)}>
+                <div className={`h-full w-full p-4 flex flex-col justify-between ${activeButton === 2 ? "bg-gray-200" : "bg-gray-200 text-black"}`}
+                style={{borderRadius: '50px'}}>
+                    <div style={{paddingLeft: "10px", paddingRight: "10px"}}>
+                        <div className='flex justify-between items-center' style={{paddingTop: '30px', paddingRight: '20px'}}>
+                            <h2 className='font-bold text-left' style={{ fontSize: '20px', paddingBottom: '20px' }}>Quiz</h2>
+                            <input type='checkbox' className='form-checkbox' checked={activeCheckbox.quiz} readOnly></input>
+                        </div>
+                        <p className='text-left'>You will set up a series of quizzes with designated right or wrong answers. At the end, the test will 
+                        calculate how many questions the user answered correctly and display the total score as the result.
+                        </p>
+                    </div>
+
+                    {/* Complexity + Engagement Metre */}
+                    <div>
+                        <div className='flex justify-left items-center mt-auto space-x-1' style={{paddingBottom: '15px', paddingLeft: '15px'}}>
+                            <p style={{paddingRight: '5px'}}>Complexity</p>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                        </div>
+                        <div className='flex justify-left items-center mt-auto space-x-1' style={{paddingBottom: '30px', paddingLeft: '15px'}}>
+                            <p style={{paddingRight: '5px'}}>Engagement</p>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-gray-400'></div>
+                        </div>
+                    </div>
                 </div>
             </button>
             
+            {/* Personality Test */} 
             <button
-            className={`flex-1 m-2 p-1 rounded ${activeButton === 3 ? "bg-blue-500" : "border-r-transparent"}`}
-            onClick={() => handleButtonClick(3)}
-            >
-                <div className={`h-full w-full p-4 rounded ${activeButton === 3 ? "bg-gray-200" : "bg-gray-200 text-black"}`}>
-                <h2 className='font-bold' >Personality Test</h2>
-                <p>You will set up personalized results to help users receal aspexts of their character or behaviour.</p>
+            className={`flex-1 m-2 p-1 ${activeButton === 3 ? "bg-blue-500" : "border-r-transparent"}`}
+            style={{ height: '600px', display: 'flex', flexDirection: 'column', borderRadius: '53px'}}
+            onClick={() => handleButtonClick(3)}>
+                <div className={`h-full w-full p-4 rounded-xl flex flex-col justify-between ${activeButton === 3 ? "bg-gray-200" : "bg-gray-200 text-black"}`}
+                style={{borderRadius: '50px'}}>
+                    <div style={{paddingLeft: "10px", paddingRight: "10px"}}>
+                        <div className='flex justify-between items-center' style={{paddingTop: '30px', paddingRight: '20px'}}>
+                            <h2 className='font-bold text-left' style={{ fontSize: '20px', paddingBottom: '20px' }}>Personality Test</h2>
+                            <input type='checkbox' className='form-checkbox' checked={activeCheckbox.personalityTest} readOnly></input>
+                        </div>
+                        <p className='text-left'>You will set up personalized results to help users of their character or behaviour. 
+                        Based on their responses, the test will generate a personalized result that refelects their personality traits, 
+                        preferences, or tendencies. While it takes more time to set up, your customers will find it engaging, fun, 
+                        and interactive!
+                        </p>
+                    </div>
+
+                    {/* Complexity + Engagement Metre */}
+                    <div>
+                        <div className='flex justify-left items-center mt-auto space-x-1' style={{paddingBottom: '15px', paddingLeft: '15px'}}>
+                            <p style={{paddingRight: '5px'}}>Complexity</p>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                        </div>
+                        <div className='flex justify-left items-center mt-auto space-x-1' style={{paddingBottom: '30px', paddingLeft: '15px'}}>
+                            <p style={{paddingRight: '5px'}}>Engagement</p>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                            <div className='w-2 h-5 bg-blue-600'></div>
+                        </div>
+                    </div>
                 </div>
             </button>
         </div>
@@ -53,9 +156,7 @@ export default function testType() {
  
         <div className="flex justify-center p-4">
             <Link href="/style">
-            <button className="bg-blue-500 text-white py-2 px-4 rounded">
-            Continue
-            </button>
+                <button className="bg-blue-500 text-white py-2 px-4" style={{borderRadius: '25px', paddingRight: '50px', paddingLeft: '50px'}}>Continue</button>
             </Link>
         </div>
     </div>
